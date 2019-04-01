0792702f40a1f88e6826b31ef7fa1699201c6654
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2019-04-01 20:03:36 +0200

    Working i18n

--- grails-app/taglib/i18n/LocaleNavbarTagLib.groovy
+++ grails-app/taglib/i18n/LocaleNavbarTagLib.groovy
@@ -3,7 +3,6 @@ package i18n
 import grails.config.Config
 import grails.core.support.GrailsConfigurationAware
 import org.springframework.context.MessageSource
-import org.springframework.web.servlet.support.RequestContextUtils
 
 class LocaleNavbarTagLib implements GrailsConfigurationAware {
 
@@ -15,6 +14,15 @@ class LocaleNavbarTagLib implements GrailsConfigurationAware {
 
     List<String> languages
 
+    Map<String, String> flagsUris = new HashMap<>()
+
+    LocaleNavbarTagLib()
+    {
+        flagsUris.put("en", "<img class=\"i18n\" src=\"https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/510px-Flag_of_the_United_Kingdom.svg.png\">")
+        flagsUris.put("fr", "<img class=\"i18n\" src=\"https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/500px-Flag_of_France.svg.png\">")
+        flagsUris.put("de", "<img class=\"i18n\" src=\"https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/510px-Flag_of_Germany.svg.png\">")
+    }
+
     @Override
     void setConfiguration(Config co) {
         languages = co.getProperty('ticket_app.languages', List)
@@ -24,10 +32,7 @@ class LocaleNavbarTagLib implements GrailsConfigurationAware {
         String uri = args.uri
 
         for ( String lang : languages ) {
-            String languageCode = "language.$lang"
-            def locale = RequestContextUtils.getLocale(request)
-            def msg = messageSource.getMessage(languageCode, [] as Object[], null, locale)
-            out << "<li><a href='${uri}?lang=${lang}'>${msg}</a></li>"
+            out << "<a href='${uri}?lang=${lang}'>${flagsUris.get(lang)}</a>"
         }
 
     }
